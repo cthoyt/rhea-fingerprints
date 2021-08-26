11aac884298158af1a6eb1d375e555d8e0005241
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-26 23:24:38 +0200

    Cleanup code

--- build.py
+++ build.py
@@ -8,23 +8,26 @@ from rdkit.Chem import AllChem, rdChemReactions
 from tqdm import tqdm
 
 HERE = pathlib.Path(__file__).parent.resolve()
-OUTPUT = HERE.joinpath("output")
-OUTPUT.mkdir(exist_ok=True, parents=True)
 
 
 def main():
     """Generate SMILES and differential reaction fingerprints for each Rhea entry."""
+    # Get the current version of Rhea using :mod:`bioversions`.
     version = bioversions.get_version("rhea")
-    module = pystow.module("bio", "rhea", version)
-    output_directory = OUTPUT.joinpath(version)
-    output_directory.mkdir(exist_ok=True, parents=True)
-    smiles_path = output_directory.joinpath("reaction_smiles.tsv")
-    fingerprints_path = output_directory.joinpath("reaction_fingerprints.pkl.gz")
 
-    tqdm.write("Downloading MDL files")
+    # Create a version-specific output directory
+    output = HERE.joinpath("output", version)
+    output.mkdir(exist_ok=True, parents=True)
+
+    # Use a version-specific PyStow directory for reproducible downloading
+    # of external resources
+    module = pystow.module("bio", "rhea", version)
+
+    tqdm.write("Downloading/unpacking reaction archive")
     directory = module.ensure_untar(
         url="ftp://ftp.expasy.org/databases/rhea/ctfiles/rhea-rxn.tar.gz",
     )
+    tqdm.write("Downloaded/unpacking reaction archive")
 
     # Search for all rxn files
     paths = list(directory.joinpath("rxn").glob("*.rxn"))
@@ -45,13 +48,13 @@ def main():
             tqdm.write(f"failed: {path.name}")
 
     rxn_df = pd.DataFrame(rows, columns=["rhea_id", "smiles"])
-    rxn_df.to_csv(smiles_path, sep="\t", index=False)
+    rxn_df.to_csv(output.joinpath("reaction_smiles.tsv"), sep="\t", index=False)
 
     # Add differential reaction fingerprints
     rxn_df["drfp"] = DrfpEncoder.encode(rxn_df.smiles)
 
     # Save full dataframe as a pickle for reuse later
-    rxn_df.to_pickle(fingerprints_path)
+    rxn_df.to_pickle(output.joinpath("reaction_fingerprints.pkl.gz"))
 
 
 if __name__ == "__main__":
