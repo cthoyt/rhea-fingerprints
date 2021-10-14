3fa62c8f2396f46430cdcc07d9e5421bb6495c54
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-14 12:53:26 +0200

    Update build.py

--- build.py
+++ build.py
@@ -14,7 +14,7 @@ from sklearn.decomposition import PCA
 from tqdm import tqdm
 
 HERE = pathlib.Path(__file__).parent.resolve()
-MODULE = pystow.module("bio", "biogrid")
+MODULE = pystow.module("bio", "rhea")
 
 
 @click.command()
