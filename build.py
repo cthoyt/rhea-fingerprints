50b90087597bb36d07987e5ac878ddf41185dcfd
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-02-01 15:42:38 +0100

    Store reaction direction, enzyme code, and class
    Closes #1

--- build.py
+++ build.py
@@ -38,12 +38,13 @@ def main(force: bool, random_state: int):
     # Create a version-specific output directory
     output = HERE.joinpath("output", version)
     output.mkdir(exist_ok=True, parents=True)
-    reaction_fingerprints_path = output.joinpath("reaction_fingerprints.tsv.gz")
+    metadata_path = output.joinpath("reaction_metadata.tsv")
+    fingerprints_path = output.joinpath("reaction_fingerprints.tsv.gz")
+    fingerprints_2d_path = output.joinpath("reaction_fingerprints_2d.tsv")
 
-    if reaction_fingerprints_path.is_file() and not force:
-        click.echo("Loading reaction dataframe")
-        fingerprint_df = pd.read_csv(reaction_fingerprints_path, sep="\t", index_col=0)
-        click.echo("Loaded reaction dataframe")
+    if metadata_path.is_file() and fingerprints_path.is_file() and not force:
+        metadata_df = pd.read_csv(metadata_path, sep="\t", index_col=0)
+        fingerprint_df = pd.read_csv(fingerprints_path, sep="\t", index_col=0)
         click.echo("::set-output name=RHEA_UPDATED::false")
     else:
         click.echo(f"::set-output name=RHEA_UPDATED::{version}")
@@ -56,6 +57,25 @@ def main(force: bool, random_state: int):
         )
         tqdm.write("Downloaded/unpacking reaction archive")
 
+        # Columns are: RHEA_ID_MASTER, RHEA_ID_LR, RHEA_ID_RL, RHEA_ID_BI
+        directions_df = MODULE.ensure_csv(
+            version,
+            url="https://ftp.expasy.org/databases/rhea/tsv/rhea-directions.tsv",
+            read_csv_kwargs=dict(dtype=str),
+        )
+        direction_master = set(directions_df["RHEA_ID_MASTER"])
+        direction_lr = dict(directions_df[["RHEA_ID_LR", "RHEA_ID_MASTER"]].values)
+        direction_rl = dict(directions_df[["RHEA_ID_RL", "RHEA_ID_MASTER"]].values)
+        direction_bi = dict(directions_df[["RHEA_ID_BI", "RHEA_ID_MASTER"]].values)
+
+        # Columns are: RHEA_ID	DIRECTION	MASTER_ID	ID
+        eccode_df = MODULE.ensure_csv(
+            version,
+            url="https://ftp.expasy.org/databases/rhea/tsv/rhea2ec.tsv",
+            read_csv_kwargs=dict(dtype=str),
+        )
+        master_to_eccode = dict(eccode_df[["MASTER_ID", "ID"]].values)
+
         # Search for all rxn files
         paths = sorted(directory.joinpath("rxn").glob("*.rxn"))
 
@@ -63,27 +83,53 @@ def main(force: bool, random_state: int):
         for path in tqdm(
             paths, unit_scale=True, unit="reaction", desc="reading reactions"
         ):
+            rhea_id = path.stem
+            if rhea_id in direction_master:
+                direction = "master"
+                master_id = rhea_id
+            elif rhea_id in direction_lr:
+                direction = "lr"
+                master_id = direction_lr[rhea_id]
+            elif rhea_id in direction_rl:
+                direction = "rl"
+                master_id = direction_rl[rhea_id]
+            elif rhea_id in direction_bi:
+                direction = "bi"
+                master_id = direction_bi[rhea_id]
+            else:
+                tqdm.write(f"failed to find the direction for {path.name}")
+                continue
+
+            ec_code = master_to_eccode.get(master_id)
+            ec_class = "Unknown" if ec_code is None else ec_code.split(".")[0]
+
             try:
                 rxn = AllChem.ReactionFromRxnFile(path.as_posix())
                 rxn.Initialize()
                 # rdChemReactions.PreprocessReaction(rxn)
                 rows.append(
                     (
-                        path.stem,
+                        rhea_id,
+                        direction,
+                        ec_code,
+                        ec_class,
                         rdChemReactions.ReactionToSmiles(rxn),
                     )
                 )
             except (ValueError, RuntimeError):
-                tqdm.write(f"failed: {path.name}")
+                tqdm.write(f"failed to parse {path.name}")
 
-        smiles_df = pd.DataFrame(rows, columns=["rhea_id", "smiles"])
-        smiles_df.to_csv(output.joinpath("reaction_smiles.tsv"), sep="\t", index=False)
+        metadata_df = pd.DataFrame(
+            rows,
+            columns=["rhea_id", "direction", "enzyme_code", "enzyme_class", "smiles"],
+        ).set_index("rhea_id")
+        metadata_df.to_csv(metadata_path, sep="\t")
 
-        # Add differential reaction fingerprints
+        click.echo("Calculating differential reaction fingerprints")
         fingerprint_df = pd.DataFrame(
-            DrfpEncoder.encode(smiles_df.smiles), index=smiles_df.rhea_id
+            DrfpEncoder.encode(metadata_df.smiles), index=metadata_df.rhea_id
         )
-        fingerprint_df.to_csv(reaction_fingerprints_path, sep="\t")
+        fingerprint_df.to_csv(fingerprints_path, sep="\t")
 
     README_PATH.write_text(index_template.render(version=version) + "\n")
 
@@ -113,7 +159,7 @@ def main(force: bool, random_state: int):
     transformed_df["cluster"] = kmeans.fit_predict(
         PCA(64, random_state=random_state).fit_transform(fingerprint_df)
     ).astype(str)
-    transformed_df.to_csv(output.joinpath("reaction_fingerprints_2d.tsv"), sep="\t")
+    transformed_df.to_csv(fingerprints_2d_path, sep="\t")
     sns.scatterplot(
         data=transformed_df,
         x="PC1",
